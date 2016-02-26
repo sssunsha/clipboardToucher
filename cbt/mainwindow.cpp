195b3cbda98a5d0a7693eeec0ea0052b7f150a8c
sha.sun <sha.sun@sonymobile.com>
2016-02-26 10:47:02 +0800

    add system clipboard test project

---
+++ cbt/mainwindow.cpp
@@ -0,0 +1,52 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    init();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::init()
+{
+    // set up title
+
+    // inint cmp
+    this->m_okButton = ui->pushButton_ok;
+    this->m_closeButton = ui->pushButton_close;
+    this->m_contentTextEdit = ui->textEdit_content;
+
+    // inin the signal and slots
+    connect(this->m_okButton, SIGNAL(clicked()), this, SLOT(handle_okButtonClicked()));
+    connect(this->m_closeButton, SIGNAL(clicked()), this, SLOT(handle_closeButtonClicked()));
+
+    // setup clipboard
+    this->m_clipboard = QApplication::clipboard();
+}
+
+// private slots
+void MainWindow::handle_okButtonClicked()
+{
+    qDebug()<< "ok button is clicked, content = " << this->m_contentTextEdit->toPlainText();
+
+    QString str = this->m_contentTextEdit->toPlainText();
+    if(str.length() > 0 && m_clipboard)
+    {
+        m_clipboard->setText(str);
+    }
+}
+
+void MainWindow::handle_closeButtonClicked()
+{
+    this->close();
+}
+
---
+++ cbt/mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QClipboard>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void handle_okButtonClicked();
+    void handle_closeButtonClicked();
+private:    
+    void init();
+
+    Ui::MainWindow *ui;
+
+    QPushButton* m_closeButton;
+    QPushButton* m_okButton;
+    QTextEdit* m_contentTextEdit;
+
+    QClipboard *m_clipboard;
+};
+
+#endif // MAINWINDOW_H
